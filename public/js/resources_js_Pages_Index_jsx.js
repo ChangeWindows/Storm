c25ccc0add6069af8679fa54ce8b96f7d34147c7
Yannick <yannick.franssen@outlook.com>
2022-01-21 00:40:44 +0100

    Setup InertiaJS

---
+++ public/js/resources_js_Pages_Index_jsx.js
@@ -0,0 +1,26 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Pages_Index_jsx"],{
+
+/***/ "./resources/js/Pages/Index.jsx":
+/*!**************************************!*\
+  !*** ./resources/js/Pages/Index.jsx ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Index)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+function _objectDestructuringEmpty(obj) { if (obj == null) throw new TypeError("Cannot destructure undefined"); }
+
+
+function Index(_ref) {
+  _objectDestructuringEmpty(_ref);
+
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "Welcome to Storm"));
+}
+
+/***/ })
+
+}]);
---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Index({ }) {
+  return (
+    <>
+      <p>Welcome to Storm</p>
+    </>
+  )
+}
